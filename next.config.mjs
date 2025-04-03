77730cf1c4d6e166e6a102631afb5e33eaae3e28
Trần Đăng Khôi <baits16@gmail.com>
2025-04-03 09:37:05 +0700

    dev: ⚡ui coding now feels like a breeze

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
+// import { createRequire } from "module";
+// const require = createRequire(import.meta.url);
+
+// const { codeInspectorPlugin } = require("code-inspector-plugin");
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  // webpack: (config, { dev, isServer }) => {
+  //   config.plugins.push(codeInspectorPlugin({ bundler: "webpack" }));
+  //   return config;
+  // },
+};
 
 export default nextConfig;
