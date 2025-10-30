403bb68e0430b74aea9c4831a02e8206b04965c4
Trần Đăng Khôi <baits16@gmail.com>
2025-10-30 14:15:10 +0700

    👍 now you can update dishes

--- src/app/(authenticated)/dashboard/dishes/_components/edit-dish-dialog.tsx
+++ src/app/(authenticated)/dashboard/dishes/_components/edit-dish-dialog.tsx
@@ -1,9 +1,12 @@
 "use client";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { useQuery, useQueryClient } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { Upload } from "lucide-react";
-import { useMemo, useRef, useState } from "react";
+import { useEffect as useLayoutEffect, useMemo, useRef, useState } from "react";
 import { useForm } from "react-hook-form";
+import { toast } from "sonner";
+import { dishApi } from "src/api-requests/dish.apis";
+import { mediaApi } from "src/api-requests/media.apis";
 import { Avatar, AvatarFallback, AvatarImage } from "src/components/ui/avatar";
 import { Button } from "src/components/ui/button";
 import {
@@ -21,18 +24,17 @@ import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "s
 import { Textarea } from "src/components/ui/textarea";
 import { DISH_STATUS, DISH_STATUS_VALUES } from "src/constants/types.constants";
 import { getVietnameseDishStatus } from "src/lib/dashboard-utils";
+import { handleErrorApi } from "src/lib/utils";
 import { TUpdateDishBody, updateDishBodySchema } from "src/validations/dish.validations";
 
 export default function EditDishDialog({
   id,
   setId,
-  onSubmitSuccess,
 }: {
   id?: number | undefined;
-  setId: (value: number | undefined) => void;
-  onSubmitSuccess?: () => void;
+  setId: React.Dispatch<React.SetStateAction<number | undefined>>;
 }) {
-  const [file, setFile] = useState<File | null>(null);
+  const [previewThumbnailFile, setPreviewThumbnailFile] = useState<File | null>(null);
   const queryClient = useQueryClient();
   const imageInputRef = useRef<HTMLInputElement | null>(null);
   const editDishForm = useForm<TUpdateDishBody>({
@@ -45,36 +47,87 @@ export default function EditDishDialog({
       status: DISH_STATUS.Unavailable,
     },
   });
-  const image = editDishForm.watch("image");
-  const name = editDishForm.watch("name");
+  const dishImage = editDishForm.watch("image");
+  const dishName = editDishForm.watch("name");
   const previewAvatarFromFile = useMemo(() => {
-    if (file) {
-      return URL.createObjectURL(file);
+    if (previewThumbnailFile) {
+      return URL.createObjectURL(previewThumbnailFile);
     }
-    return image;
-  }, [file, image]);
+    return dishImage;
+  }, [previewThumbnailFile, dishImage]);
 
-  // const { data: dishDetail } = useQuery({
-  //   queryKey: ["dish", id],
-  //   queryFn: () => dishApi.getDishDetail(id!),
-  //   enabled: Boolean(id),
-  // });
+  const { data: dishDetail } = useQuery({
+    queryKey: ["dish", id],
+    queryFn: () => dishApi.getDishDetail(id!),
+    enabled: Boolean(id),
+    staleTime: 10000,
+  });
+
+  const uploadImageMutation = useMutation({
+    mutationKey: ["upload-image"],
+    mutationFn: (body: FormData) => mediaApi.uploadImage(body),
+  });
+
+  const editDishMutation = useMutation({
+    mutationFn: (body: TUpdateDishBody) => dishApi.updateDish(id!, body),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["dishes"] });
+      setId(undefined);
+      toast.success(
+        <p>
+          Món ăn <span className="font-bold">{dishName}</span> đã được cập nhật thành công
+        </p>,
+      );
+    },
+  })
+
+  const dish = dishDetail?.payload.data;
 
   const handleReset = () => {
     editDishForm.reset({
-      name: "",
-      description: "",
-      price: 0,
-      image: "",
-      status: DISH_STATUS.Unavailable,
+      name: dish?.name,
+      description: dish?.description,
+      price: dish?.price,
+      image: dish?.image,
+      status: dish?.status,
     });
     // Clear preview image
-    setFile(null);
+    setPreviewThumbnailFile(null);
   };
 
-  const handleEditDish = editDishForm.handleSubmit((data) => {
-    console.log(data);
-  })
+  const handleEditDish = editDishForm.handleSubmit(async (data) => {
+    let newThumbnailUrl = null;
+    try {
+      if (previewThumbnailFile) {
+        const formData = new FormData();
+        formData.append("file", previewThumbnailFile);
+        const result = await uploadImageMutation.mutateAsync(formData);
+        newThumbnailUrl = result.payload.data;
+      }
+      editDishMutation.mutate({
+        ...data,
+        image: newThumbnailUrl ?? dishImage,
+      });
+    } catch (err) {
+      handleErrorApi({
+        error: err,
+        setError: editDishForm.setError,
+        defaultMessage: "Có lỗi xảy ra khi cập nhật thông tin món ăn",
+      });
+    }
+  });
+
+  useLayoutEffect(() => {
+    if (dish) {
+      editDishForm.reset({
+        name: dish.name,
+        description: dish.description,
+        price: dish.price,
+        image: dish.image,
+        status: dish.status,
+      });
+    }
+  }, [dish, editDishForm])
 
   return (
     <Dialog
@@ -107,7 +160,7 @@ export default function EditDishDialog({
                     <div className="flex items-start justify-start gap-2">
                       <Avatar className="aspect-square h-[100px] w-[100px] rounded-md object-cover">
                         <AvatarImage src={previewAvatarFromFile as string} />
-                        <AvatarFallback className="rounded-none">{name || "Avatar"}</AvatarFallback>
+                        <AvatarFallback className="rounded-none">{dishName || "Avatar"}</AvatarFallback>
                       </Avatar>
                       <input
                         type="file"
@@ -116,7 +169,7 @@ export default function EditDishDialog({
                         onChange={(e) => {
                           const file = e.target.files?.[0];
                           if (file) {
-                            setFile(file);
+                            setPreviewThumbnailFile(file);
                             field.onChange("http://localhost:3000/" + file.name);
                           }
                         }}
